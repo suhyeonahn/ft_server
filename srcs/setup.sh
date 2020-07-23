d8a2b43a0d56ebe54e909bc5803ac2f258da6c43
Suhyeon AHN <54574221+suhyeonahn@users.noreply.github.com>
2020-07-23 18:34:15 +0200

    Create setup.sh

---
+++ srcs/setup.sh
@@ -0,0 +1,22 @@
+
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
+rm /etc/nginx/sites-enabled/default
+
+echo "<?php phpinfo(); ?>" >> /var/www/html/phpinfo.php
+
+#service mysql start
+#echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
+#echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+#echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
+#echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+
+chown -R www-data:www-data /var
+chmod -R 755 /var
+
+
+#SERVICE STARTER
+#service mysql restart
+service php7.3-fpm restart
+service nginx restart
+bash
