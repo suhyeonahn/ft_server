37236a15244285c812facd1ecb72b0f35a9a00aa
Suhyeon AHN <54574221+suhyeonahn@users.noreply.github.com>
2020-07-23 23:54:24 +0200

    Add files via upload

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -10,6 +10,12 @@ echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTIO
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
+#PHPADMIN INSTALL
+#wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-english.tar.gz
+#mkdir /var/www/html/phpmyadmin
+#tar xzf phpMyAdmin-4.9.0.1-english.tar.gz --strip-components=1 -C /var/www/localhost/phpmyadmin
+#cp /root/config.inc.php /var/www/localhost/phpmyadmin/
+
 chown -R www-data:www-data /var
 chmod -R 755 /var
 
