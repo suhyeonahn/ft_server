0df76978ef42097c195642db27dcd8218e11d13f
Suhyeon AHN <54574221+suhyeonahn@users.noreply.github.com>
2020-07-29 22:52:51 +0200

    Update setup.sh

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -5,6 +5,7 @@ mkdir -p /run/php && touch /run/php/php7.3-fpm.sock
 echo "<?php phpinfo(); ?>" >> /var/www/html/phpinfo.php
 
 service mysql start
+chown -R mysql:mysql /var/lib/mysql/wordpress
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
@@ -16,8 +17,8 @@ echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 #tar xzf phpMyAdmin-4.9.0.1-english.tar.gz --strip-components=1 -C /var/www/localhost/phpmyadmin
 #cp /root/config.inc.php /var/www/localhost/phpmyadmin/
 
-chown -R www-data:www-data /var
-chmod -R 755 /var
+chown -R www-data:www-data /var/www
+chmod -R 755 /var/www
 
 #SERVICE STARTER
 service php7.3-fpm restart
